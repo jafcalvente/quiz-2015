0c8d2662483e2ea2320d2f91ebf45c1febd9b21c
Jose Fuentes <jafcalvente@outlook.com>
2015-06-28 21:08:44 +0200

    Editar preguntas. Uso de method-override.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -103,6 +103,30 @@ exports.create = function(req, res) {
 	});
 };
 
+// GET /quizes/:quizId/edit
+exports.edit = function(req, res) {
+
+	var quiz = req.quiz; // Autoload de instancia de quiz
+	res.render('quizes/edit', { quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:quizId
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(function(err) {
+		if(err) {
+			res.render('quizes/edit', { quiz: req.quiz, errors: err.errors });
+		} else {
+			req.quiz.save({ fields: ['pregunta', 'respuesta']})
+			.then(function() { res.redirect('/quizes') });
+		}
+	});
+};
+
 // GET /author
 exports.author = function(req, res) {
 	res.render('author', { author: 'Jose A. Fuentes'});
