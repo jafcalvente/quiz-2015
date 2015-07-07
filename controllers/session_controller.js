91e581fbf302f5a4fa7d7837a4ff542fbf4faa23
Jose Fuentes <jafcalvente@outlook.com>
2015-07-07 20:31:17 +0200

    Añadida autorización a la gestión de quizes

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,6 +1,15 @@
 // Importamos el módulo de autenticación
 var userController = require('./user_controller');
 
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next) {
+	if (req.session.user) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
 // GET /login - Formulario de login
 exports.new = function(req, res) {
 
